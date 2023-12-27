6724252e48a241dbf422673fe21f4b00eb904182
vhdnet <vhdnet@gmail.com>
2023-12-27 14:31:53 +0330

    optimizing

--- index.js
+++ index.js
@@ -2,34 +2,37 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
-let leadsFromLocalstorage = JSON.parse(localStorage.getItem("myLeads"));
+const deletBtn = document.getElementById("delete-btn");
+const leadsFromLocalstorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalstorage) {
   myLeads = leadsFromLocalstorage;
-  renderLeads();
+  render(myLeads);
 }
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-
-  console.log(localStorage.getItem("myLeads"));
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target="_blank" href ="${myLeads[i]}"> 
-        ${myLeads[i]} 
+        <a target="_blank" href ="${leads[i]}"> 
+        ${leads[i]} 
         </a>
       </li>
       `;
   }
   ulEl.innerHTML = listItems;
 }
+
+deletBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
